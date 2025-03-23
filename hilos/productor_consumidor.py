d127b5e1d8e230974451c8ad7e7e9d727fdd16c6
Andrés Palacio <andres.palacio@utp.edu.co>
2025-03-23 18:44:13 -0500

    fix non-stop validation

--- hilos/productor_consumidor.py
+++ hilos/productor_consumidor.py
@@ -8,30 +8,31 @@ logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-9s) %(message)s'
 
 BUF_SIZE = 10
 q = queue.Queue(BUF_SIZE)
+stop_event = threading.Event()
 
 class HiloProductor(threading.Thread):
-    def __init__(self, name=None):
-        super(HiloProductor, self).__init__()
-        self.name = name
+  def __init__(self, name=None):
+    super(HiloProductor, self).__init__()
+    self.name = name
 
-    def run(self):
-        while True:
-            item = random.randint(1, 10)
-            q.put(item)  # Bloquea si la cola está llena
-            logging.debug(f'Insertando "{item}" : {q.qsize()} elementos en la cola')
-            time.sleep(random.random())
+  def run(self):
+    while not stop_event.is_set():
+      item = random.randint(1, 10)
+      q.put(item)  # Bloquea si la cola está llena
+      logging.debug(f'Insertando "{item}" : {q.qsize()} elementos en la cola')
+      time.sleep(random.random())
 
 class HiloConsumidor(threading.Thread):
-    def __init__(self, name=None):
-        super(HiloConsumidor, self).__init__()
-        self.name = name
+  def __init__(self, name=None):
+    super(HiloConsumidor, self).__init__()
+    self.name = name
 
-    def run(self):
-        while True:
-            item = q.get()  # Bloquea si la cola está vacía
-            logging.debug(f'Sacando "{item}" : {q.qsize()} elementos en la cola')
-            time.sleep(random.random())
-            q.task_done()  # Indica que el ítem fue procesado
+  def run(self):
+    while not stop_event.is_set() or not q.empty():
+      item = q.get()  # Bloquea si la cola está vacía
+      logging.debug(f'Sacando "{item}" : {q.qsize()} elementos en la cola')
+      time.sleep(random.random())
+      q.task_done()  # Indica que el ítem fue procesado
 
 # Crear y lanzar hilos
 p1 = HiloProductor(name='Productor1')
@@ -42,11 +43,14 @@ p1.start()
 p2.start()
 c1.start()
 
+# Ejecutar por un tiempo determinado
+time.sleep(10)
+stop_event.set()
+
 # Esperar a que la cola se vacíe
 q.join()
 
 # Detener los hilos
-
 p1.join()
 p2.join()
 c1.join()
